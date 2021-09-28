37f2b4d9f7676ad481724297a54cfa666e0fe91e
Seogeurim <geulims@naver.com>
2021-09-28 17:35:06 +0900

    [chore] add eslint & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import React from 'react';
 import './styles/index.scss';
 
 const App: React.FC = () => {
-  return (
-    <div>This is React App</div>
-  );
+  return <div>This is React App</div>;
 };
 
 export default App;
