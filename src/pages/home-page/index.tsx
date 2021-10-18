bbb3d8c8deb4d2a271c352e50ee7e1c6f99f1d69
Seogeurim <geulims@naver.com>
2021-10-19 00:07:03 +0900

    [feat] update home page content

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -1,8 +1,21 @@
 import React, { FC } from 'react';
 import { Layout } from '../../components';
 
+import icon from '../../assets/code.png';
+import './style.scss';
+
 const HomePage: FC = () => {
-  return <Layout>This is Home Page.</Layout>;
+  return (
+    <Layout>
+      <section className="welcome">
+        <h1>React TypeScript Boilerplate</h1>
+        <p>
+          <img src={icon} alt="icon" />
+          This is a React X TypeScript boilerplate using webpack v5.
+        </p>
+      </section>
+    </Layout>
+  );
 };
 
 export default HomePage;
